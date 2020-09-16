3fdb1dfb47f8d4175d0887efc63848caefc4bac4
cmzhong <cheriemzhong@gmail.com>
2020-09-16 13:21:19 -0700

    Add React Router Routes to main App component

--- client/App.tsx
+++ client/App.tsx
@@ -1,5 +1,14 @@
 import * as React from 'react';
 import { hot } from "react-hot-loader/root";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from './components/About';
+import Map from './components/Map';
+
 
 interface Props {
 }
@@ -8,9 +17,20 @@ class App extends React.Component<Props> {
   render() {
     return (
       <>
-        <h1>
-          Helllooo?
-        </h1>
+        <h1>minlog</h1>
+        <Router>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+
+          <Switch>
+            <Route path ="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <Map />
+            </Route>
+          </Switch>
+        </Router>
       </>
     );
   }
